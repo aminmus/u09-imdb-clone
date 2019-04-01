0db11efbcfbce37d8f170e64fd7732870d098e50
Daniel Hernqvist <daniel.hernqvist@chasacademy.se>
2019-04-01 12:13:09 +0200

    Add margin to specified buttons

--- resources/views/searchresult.blade.php
+++ resources/views/searchresult.blade.php
@@ -11,14 +11,14 @@
                 <h1>{{$result->title}} </h1>
             </div>
             <div class="card-body justify-content-center">
-                <p>  {{$result->id}}<p>
+                <!-- <p>  {{$result->id}}<p> -->
                 <img src="http://image.tmdb.org/t/p/w185/<?php echo $result->poster_path;?>">
                 <form method="POST" action="{{ action('WatchlistController@store') }}">
                 <input name="_token" type="hidden" value="{{ csrf_token() }}"/>
                 <input name="movie_id" type="hidden" value="<?php echo $result->id;?>"/>
                 <input name="title" type="hidden" value="<?php echo $result->title;?>"/>
                 <input name="poster_path" type="hidden" value="<?php echo $result->poster_path;?>"/>
-                <button type="submit">Save Movie</button>
+                <button class="my-3" type="submit">Save Movie</button>
                 </form>
             </div>
         </div>
--- resources/views/showselectedwatchlist.blade.php
+++ resources/views/showselectedwatchlist.blade.php
@@ -1,13 +1,23 @@
+@extends('welcome')
 
-
-
+@section('content')
 @foreach ($filmsFromWatchlist as $film)
-    <h1>{{$film->movie_id}}</h1>
-    <h1>{{$film->title}}</h1>
-    <img src="http://image.tmdb.org/t/p/w185/<?php echo $film->poster_path;?>">
+
+    <div class="row justify-content-center text-center my-5">
+      <div class="col-md-8 text-center">
+        <div class="card card-default">
+          <div class="card-header">
+            <h1>{{$film->title}}</h1>
+          </div>
+          <div class="card-body justify-content-center">
+            <img src="http://image.tmdb.org/t/p/w185/<?php echo $film->poster_path;?>">
+          </div>
+        </div>
+      </div>
+    </div>
 
    
 
 @endforeach
 
-
+@endsection
