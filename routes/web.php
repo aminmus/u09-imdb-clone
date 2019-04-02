b244a4e0b9ebb94b09cd28b1c5ea926889b3e837
Amin Mushehid <amin.mushehid@chasacademy.se>
2019-04-03 01:08:20 +0200

    Add empty controller, view and route for profile page

--- routes/web.php
+++ routes/web.php
@@ -49,3 +49,6 @@ Route::post('morereviews', 'ReviewController@postReview');
 // Authentication Routes (added by default by laravel)
 Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
+
+// Profile
+Route::get('/profile', 'ProfileController@showProfile');
