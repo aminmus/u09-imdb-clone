c00d4b5a9657a2bad98471a9d0b9b247fce9f1bc
Leandro Da Souza <leandrodasouza@Leandros-MacBook-Pro.local>
2019-04-03 10:47:26 +0200

    add functionality to edit modal button

--- resources/views/selectedfilm.blade.php
+++ resources/views/selectedfilm.blade.php
@@ -67,30 +67,44 @@
     <button type="submit" class="btn btn-primary" data-toggle="modal" data-target="#editModal">Edit</button>
     <hr>
 @endif
- @endforeach
- 
- 
+
+
 <!-- edit modal -->
 <div class="modal fade" id="editModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Edit</h5>
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Edit</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+            <form method="POST" action="/reviews/{{$review->id}}" >
+                        @csrf
+                        @method('PUT')
+                {{-- <input name="movie_id" type="hidden" class="mb-2"value="{{$review->id}}"/> --}}
+                <input type="text-area" class="form-control" id="formGroupExampleInput2" name="content" placeholder="review" class="mb-2" value="{{$review->content}}">
+                        <select class="custom-select col-md-2 mt-1" id="inputGroupSelect04" name="rating" aria-label="Example select with button addon">
+                        <option selected="{{$review->rating}}">Rating</option>
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
+                        </select>
+                        
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-primary">Save changes</button>
+                    </div>
+                </form>
+            </div>
         </div>
-        <div class="modal-body">
-          ...
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-          <button type="button" class="btn btn-primary">Save changes</button>
-        </div>
-      </div>
     </div>
-  </div>
-
+    
+    @endforeach
  <!-- Här behövs det visas kommentarer för specifik film
  Ladda in reviews. -->
 @auth
