12eaa5c8d13b4593ee05a88faca94b8b8ef0ea6d
Amin Mushehid <amin.mushehid@chasacademy.se>
2019-04-03 14:24:43 +0200

    Show admin link if user is admin in profile, add watchlists link in profile

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -4,14 +4,19 @@
 <div class="container">
     <div class="row">
         <div class="col-md-3">
-            <h2>{{Auth::user()->name}}</h2>
+            <h1>{{Auth::user()->name}}</h2>
 
             <!-- User Menu -->
             <div>
                 <ul>
-                    <li><a href="#">Watchlists</a></li>
+                    <li><a href="{{ url('/watchlist') }}">Watchlists</a></li>
                     <li><a href="#">Reviews</a></li>
                     <li><a href="#">Account Settings</a></li>
+
+                    @if (Auth::user()->is_admin)
+                    <li><a href="#">Administration</a></li>
+                    @endif
+
                 </ul>
             </div>
         </div>
