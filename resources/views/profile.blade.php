44ebb1882d1f15c0e027852721583af1c7a1adb5
Amin Mushehid <amin.mushehid@chasacademy.se>
2019-04-03 16:18:48 +0200

    Add comments and clarify that views for hrefs in profile are in progress and should be updated later

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -6,19 +6,21 @@
         <div class="col-md-3">
             <h1>{{Auth::user()->name}}</h2>
 
-            <!-- User Menu -->
-            <div>
-                <ul>
-                    <li><a href="{{ url('/watchlist') }}">Watchlists</a></li>
-                    <li><a href="#">Reviews</a></li>
-                    <li><a href="#">Account Settings</a></li>
+                <!-- User Menu -->
+                <div>
+                    <ul>
+                        <li><a href="{{ url('/watchlist') }}">Watchlists</a></li>
 
-                    @if (Auth::user()->is_admin)
-                    <li><a href="#">Administration</a></li>
-                    @endif
+                        <!-- TODO: Add hrefs to views below when they are created  -->
+                        <li><a href="#">Reviews (coming soon)</a></li>
+                        <li><a href="#">Account Settings (coming soon)</a></li>
 
-                </ul>
-            </div>
+                        @if (Auth::user()->is_admin)
+                        <li><a href="#">Administration (coming soon)</a></li>
+                        @endif
+
+                    </ul>
+                </div>
         </div>
     </div>
 </div>
