0bb18f902d4b0eb006b7df3730ae723fc0ae6a44
Daniel Hernqvist <daniel.hernqvist@chasacademy.se>
2019-04-12 10:25:33 +0200

    Fixed styling on Actors-page

--- resources/views/selectedactor.blade.php
+++ resources/views/selectedactor.blade.php
@@ -1,15 +1,37 @@
+@extends('welcome')
 
-<img src="http://image.tmdb.org/t/p/w185/<?php echo $actor->profile_path;?>" alt="">
-<h3>{{$actor->name}}</h3>
-<p>{{$actor->birthday}}</p>
-<p>{{$actor->biography}}</p>
-<p>{{$actor->place_of_birth}}</p>
+@section('content')
+    <div class="container my-5">
+      <div class="mx-3">
+        <img src="http://image.tmdb.org/t/p/w185/<?php echo $actor->profile_path;?>" alt="">
+      </div>
+      <div class="mx-3">
+        <h3>{{$actor->name}}</h3>
+        <p>{{$actor->birthday}}</p>
+        <p>{{$actor->biography}}</p>
+        <p><b>{{$actor->place_of_birth}}</b></p>
+      </div>
+      </div>
 
+
+
+
+<div class="col">
+<div class="">
 @foreach ($movies as $movie)
-    <p>{{$movie->character}}</p>
-    <p>{{$movie->release_date}}</p>
-    <p></p>
-    <p>{{$movie->title}}</p>
-    <p>{{$movie->overview}}</p>
-    <a href="{{ url('selectedfilm/' .$movie->id. '/') }}"><img src="http://image.tmdb.org/t/p/w185/<?php echo $movie->poster_path;?>" alt=""></a>
+<div class="row text-left">
+  <ul class="row">
+    <li>
+      <a class="mx-3 row" href="{{ url('selectedfilm/' .$movie->id. '/') }}">
+      <p class="mx-3">{{$movie->release_date}}</p>
+      <p>{{$movie->title}}</p>
+      </a>
+      </li>
+      <p class="">{{$movie->character}}</p>
+  </ul>
+  </div>
 @endforeach
+</div>
+</div>
+
+@endsection
