10a889c2366ebe6d4c35f49213b4150fe94194c4
Daniel Hernqvist <daniel.hernqvist@chasacademy.se>
2019-04-12 10:33:55 +0200

    Add center-styling to watchlist-create-component

--- resources/views/watchlistform.blade.php
+++ resources/views/watchlistform.blade.php
@@ -1,12 +1,13 @@
 @extends('welcome')
 
 @section('content')
-    
-<h1>Create Watchlist</h1>
+<div class="my-5 column text-center justify-content-center">   
+<h1 class="my-5">Create Watchlist</h1>
 <form method="POST" action="/watchlist">
     @csrf
     <label for="name">Genre name:</label>
     <input type="text" name="name" placeholder="genre">
     <button type="submit">Submit</button>
 </form>
+</div>
 @endsection
