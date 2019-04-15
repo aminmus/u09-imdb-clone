943edfac03395dfede72c5be740283f10a5f2a71
Daniel Hernqvist <daniel.hernqvist@chasacademy.se>
2019-04-15 11:07:14 +0200

    Implement 3 separate columns in reviews to make it more UI friendly. Also add center-styling

--- resources/views/reviews/showReviews.blade.php
+++ resources/views/reviews/showReviews.blade.php
@@ -1,9 +1,11 @@
+<div class="my-5">
 @foreach ($reviews as $review)
+<p>{{$review->created_at}}</p>
+<div class="row my-4" id="review-container-{{$review->id}}">
 
-<div class="row my-5" id="review-container-{{$review->id}}">
 <div class="col-md-2">
-    <h2 class="review-rating">RATING: {{$review->rating}}</h2>
-    <h2 class="review-user_id">USER-NAME: {{$review->user_id}}</h2>
+    <h4 class="review-rating">RATING: {{$review->rating}}</h2>
+    <h4 class="review-user_id">USER-NAME: {{$review->user_id}}</h2>
 </div>
 <div class="col-md-8">
     <h2 class="review-content">{{$review->content}}</h2>
@@ -21,13 +23,13 @@
     <button type="submit" class="btn btn-primary" data-toggle="modal" data-target="#editModal" value={{$review->id}}
         onClick="handleClick(this.value)">Edit</button>
 </div>
-    <hr>
 
     @endif
     @endauth
 
+
+    <hr>
 </div>
-
 @endforeach
-
+</div>
 @include('reviews.editModal')
