749b39abe6c24918df75c9d7ed3880e428c0d32e
Daniel Hernqvist <daniel.hernqvist@chasacademy.se>
2019-04-16 12:21:40 +0200

    Add styling to profile dashboard

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,27 +1,83 @@
 @extends('welcome')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-3">
-            <h1>{{Auth::user()->name}}</h2>
 
-                <!-- User Menu -->
-                <div>
-                    <ul>
-                        <li><a href="{{ url('/watchlist') }}">Watchlists</a></li>
+<h1 class="py-5 px-5 text-center">Profile Dashboard {{Auth::user()->name}}</h1>
 
-                        <!-- TODO: Add hrefs to views below when they are created  -->
-                        <li><a href="{{ url('/profile/reviews')}}">Reviews</a></li>
-                        <li><a href="#">Account Settings (coming soon)</a></li>
+<!-- Card deck -->
+<div class="card-deck py-5 px-5">
 
-                        @if (Auth::user()->is_admin)
-                        <li><a href="/admin">Administration</a></li>
-                        @endif
+  <!-- Card -->
+  <div class="card mb-4">
 
-                    </ul>
-                </div>
-        </div>
+    <!--Card content-->
+    <div class="card-body">
+
+      <!--Title-->
+      <h4 class="card-title"><li><a href="{{ url('/watchlist') }}">Watchlists</a></li></h4>
+      <!--Text-->
+      <p class="card-text">Browse, edit and update your personal watchlists here!</p>
+      <!-- Provides extra visual weight and identifies the primary action in a set of buttons -->
+      <button type="button" class="btn btn-light-blue btn-md">---></button>
+
     </div>
+
+  </div>
+  <!-- Card -->
+
+  <!-- Card -->
+  <div class="card mb-4">
+
+
+    <!--Card content-->
+    <div class="card-body">
+
+      <!--Title-->
+      <h4 class="card-title"><li><a href="{{ url('/profile/reviews')}}">Reviews</a></li></h4>
+      <!--Text-->
+      <p class="card-text">Overview and manage your own reviews for specific movies here!</p>
+      <!-- Provides extra visual weight and identifies the primary action in a set of buttons -->
+      <button type="button" class="btn btn-light-blue btn-md">---></button>
+
+    </div>
+
+  </div>
+  <!-- Card -->
+
+  <!-- Card -->
+  <div class="card mb-4">
+
+    <!--Card content-->
+    <div class="card-body">
+
+      <!--Title-->
+      <h4 class="card-title"><li><a href="#">Account Settings (coming soon)</a></li></h4>
+      <!--Text-->
+      <p class="card-text">Personal interface settings and more!</p>
+      <!-- Provides extra visual weight and identifies the primary action in a set of buttons -->
+      <button type="button" class="btn btn-light-blue btn-md">---></button>
+
+    </div>
+
+  </div>
+  <!-- Card -->
+
+@if (Auth::user()->is_admin)
+  <!-- Card -->
+  <div class="card mb-4"> 
+    <!--Card content-->
+    <div class="card-body">
+      <!--Title-->
+      <h4 class="card-title"><li><a href="/admin">Administration</a></li></h4>
+      <!--Text-->
+      <p class="card-text">Administration controls and settings!</p>
+      <!-- Provides extra visual weight and identifies the primary action in a set of buttons -->
+      <button type="button" class="btn btn-light-blue btn-md">---></button>
+    </div>
+  </div>
+  <!-- Card -->
+  @endif
 </div>
+<!-- Card deck -->
+
 @endsection
