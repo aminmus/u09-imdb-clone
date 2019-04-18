e8679297438efbf8aecef1309ec19a8ca434c748
Amin Mushehid <amin.mushehid@chasacademy.se>
2019-04-18 16:31:36 +0200

    Add validation to updateUsers() in AdminController.
    Also clean up comments and add TODO comments for remaining methods that need validation in AdminController.

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Support\Facades\Input;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Input;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
 use App\Watchlist;
 use App\Review;
 use App\User;
@@ -24,10 +26,6 @@ class AdminController extends Controller
 
     public function deleteReview($id)
     {
-        // $input = Input::all();
-        // $reviews = array_slice($input, 2);
-        // Review::whereIn('id', $reviews)->delete();
-        // return redirect('/admin')->with('success', 'Review Deleted!');
         $review = Review::find($id);
         $review->delete();
         return back()->with('success', 'Review Deleted!');
@@ -35,11 +33,6 @@ class AdminController extends Controller
 
     public function deleteUser(Request $request, $id)
     {
-        // $input = Input::all();
-        // $users = array_slice($input, 2);
-        // User::whereIn('id', $users)->delete();
-        // return redirect('/admin')->with('success', 'User Deleted!');
-
         $user = User::find($id);
         $user->delete();
         return back()->with('success', 'User Deleted!');
@@ -47,19 +40,9 @@ class AdminController extends Controller
 
     public function deleteWatchlist(Request $request, $id)
     {
-        // $input = Input::all();
-        // $watchlists = array_slice($input, 2);
-        
-        // // Remove the relation in pivot table (required for next step)
-        // Filmwatchlist::whereIn('watchlist_id', $watchlists)->delete();
-
-        // Watchlist::whereIn('id', $watchlists)->delete();
-        // return redirect('/admin')->with('success', 'Watchlist Deleted!');
-
         $watchlist = Watchlist::find($id);
         $watchlist->delete();
-        // $filmWatchlist = Filmwatchlist::whereIn('watchlist_id', $id);
-        // $filmWatchlist->delete();
+
         return back()->with('success', 'Watchlist Deleted');
     }
 
@@ -85,8 +68,8 @@ class AdminController extends Controller
     {
         // Validation
         $request->validate([
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'name' => ['required', 'string', 'max:191'],
+            'email' => ['required', 'string', 'email', 'max:191', 'unique:users'],
             'password' => ['required', 'string', 'min:6', 'confirmed'],
         ]);
 
@@ -107,6 +90,8 @@ class AdminController extends Controller
 
     public function updateReview(Request $request, $id)
     {
+        // TODO: Add validation
+
         $review = Review::find($id);
 
         $review->id = Input::get('id');
@@ -123,18 +108,26 @@ class AdminController extends Controller
     {
         $user = User::find($id);
 
+        // Validation
+        Validator::make($request->all(), [
+            'id' => ['required', 'numeric', 'unique:users', 'max:191', Rule::unique('users')->ignore($user->id)],
+            'name' => ['required', 'string', 'max:191'],
+            'email' => ['required', 'string', 'email', 'max:191', Rule::unique('users')->ignore($user->email)],
+        ]);
+
         $user->id = $request->id;
         $user->is_admin = $request->is_admin;
         $user->name = $request->name;
         $user->email = $request->email;
-        $user->email_verified_at = $request->email_verified_at;
-
         $user->save();
+
         return back()->with('success', 'User Updated');
     }
 
     public function updateWatchlist(Request $request, $id)
     {
+        // TODO: Add validation
+
         $watchlist = Watchlist::find($id);
         $watchlist->id = $request->id;
         $watchlist->name = $request->name;
