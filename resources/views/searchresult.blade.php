106ba5afc8b23fd87582b29157f7ae8cc8d8bec6
Ruben Salas Amner <ruben.salas.amner@chasacademy.se>
2019-04-18 09:46:20 +0200

    remove files that were added by mistake

--- resources/views/searchresult.blade.php
+++ resources/views/searchresult.blade.php
@@ -2,24 +2,20 @@
 
 @section('content')
 @if(isset($body->results))
+    <div class="row justify-content-center text-center my-5">
     @foreach ($body->results as $result)
-
-    
-   <div class="row justify-content-center text-center my-5">
-    <div class="col-md-8 text-center">
-        <div class="card card-default">
-            <div class="card-header">
-                <h1>{{$result->title}} </h1>
-            </div>
-            <div class="card-body justify-content-center">
-            <a href="{{ url("selectedfilm/{$result->id}") }}"><img
-                src="{{ url("http://image.tmdb.org/t/p/w185/{$result->poster_path}") }}"></a>
-            </div>
+        <div class="col-md-2 text-center">
+            <a href="{{ url("selectedfilm/{$result->id}") }}">
+                <img
+                src="{{ url("http://image.tmdb.org/t/p/w185/{$result->poster_path}") }}">
+            </a>
+            <h4>{{$result->title}} </h4>
+        
         </div>
-    </div>
-   </div>
+  
 
     @endforeach
+    </div>
 @else
     <h1>Didnt work</h1>
 @endif 
