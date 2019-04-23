dbe877c9fcc30415c3ddb76f7362b3b1b9dd9b45
Amin Mushehid <amin.mushehid@chasacademy.se>
2019-04-23 21:54:19 +0200

    Add cards for admin navigation page.

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -8,10 +8,37 @@
         <h1>Admin</h1>
     </div>
 
-    <a href="/admin/reviews">Reviews</a>
-    <a href="/admin/users">Users</a>
-    <a href="/admin/watchlists">Watchlists</a>
+    <div class="card-deck">
+        <div class="card mb-4">
+            <a href="/admin/reviews">
+                <div class="card-body">
+                    <h4 class="card-title">Reviews</h4>
+                    <p class="card-text">View Reviews</p>
+                </div>
+            </a>
+        </div>
 
-</div>
+        <div class="card mb-4">
+            <a href="/admin/users">
+                <div class="card-body">
+                    <h4 class="card-title">Users</h4>
+                    <p class="card-text">View Users</p>
+                </div>
+            </a>
+        </div>
 
-@endsection
+        <div class="card mb-4">
+            <a href="/admin/watchlists">
+                <div class="card-body">
+                    <h4 class="card-title">Watchlists</h4>
+                    <p class="card-text">View Watchlists</p>
+                </div>
+            </a>
+        </div>
+
+
+
+
+    </div>
+
+    @endsection
